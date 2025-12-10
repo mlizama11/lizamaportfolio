2dd0721e38ceb06d1d5f0d72b6b50e8df1c122b9
Mauricio Lizama <mlizamaoliger@gmail.com>
2025-12-10 17:36:57 +0100

    update metadata

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -5,7 +5,8 @@ export const siteTitle: string = 'Mauricio Lizama - Portfolio';
 export const siteDescription: string =
   'Welcome to my portfolio website where I showcase my projects, skills, and experience as a developer.';
 export const siteUrl: string = 'https://mlizama.netlify.app';
-export const siteImage: string = `${siteUrl}/og-image.png`;
+export const siteImage: string = `${siteUrl}/opengraph-image.jpg`;
+
 // Navigation Links
 export const links: Link[] = [
   { href: '/#home', label: 'Intro' },
@@ -15,5 +16,5 @@ export const links: Link[] = [
   { href: '/#education', label: 'Education' },
   { href: '/#stack', label: 'Stack' },
   { href: '/#blog', label: 'Blog' },
-  { href: '/#contact', label: 'Contact' }
+  { href: '#contact', label: 'Contact' }
 ];
