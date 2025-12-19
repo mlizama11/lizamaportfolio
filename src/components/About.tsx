ee4fb6d1c64b591779b39fbdec86052f89799b12
Mauricio Lizama <mlizamaoliger@gmail.com>
2025-12-19 15:33:37 +0100

    updated title

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,11 +5,11 @@ export default function About() {
     <Section id="about">
       <SectionTitle className="text-3xl font-bold">About Me</SectionTitle>
       <p>
-        I am a Web Developer with four years of hands-on experience in software
-        application development. I am enthusiastic about communication, and I
-        firmly believe that teamwork, fun, and commitment are key to achieving
-        successful results. I am also a trained journalist with a special
-        motivation for the use of new technologies.
+        I am a Full Stack Web and Software Developer with four years of hands-on
+        experience in software application development. I am enthusiastic about
+        communication, and I firmly believe that teamwork, fun, and commitment
+        are key to achieving successful results. I am also a trained journalist
+        with a special motivation for the use of new technologies.
       </p>
     </Section>
   );
