10c81528120da0183f7570d626d5eb10a75b76c7
Mauricio Lizama <mlizamaoliger@gmail.com>
2026-02-06 17:00:18 +0100

    sorted blog

--- src/components/BlogPostsList.tsx
+++ src/components/BlogPostsList.tsx
@@ -10,48 +10,54 @@ export async function BlogPostsList() {
 
   return (
     <div className="mt-6 space-y-4">
-      {blogPosts.slice(0, 4).map((post) => {
-        const formattedDate = formatDate(post.date);
-        const formattedUpdatedAt = formatDate(post.updatedAt);
-        return (
-          <div
-            key={post.id}
-            className="flex items-start gap-4 max-[700px]:flex-col"
-          >
-            {post.image && (
-              <Image
-                src={post.image.src}
-                alt={post.image.alt}
-                width={150}
-                height={150}
-                className="shrink-0 rounded"
-                loading="eager"
-              />
-            )}
-            <div className="flex flex-col gap-2">
-              <h3 className="font-semibold">{post.title}</h3>
-              <div className="flex items-center gap-1 max-[700px]:flex-col max-[700px]:items-start">
-                <span className="text-xs text-gray-500 dark:text-gray-400">
-                  Published on {formattedDate}
-                </span>
-                <span className="max-[700px]:hidden">|</span>
-                <span className="text-xs text-gray-500 dark:text-gray-400">
-                  Updated on {formattedUpdatedAt}
-                </span>
-              </div>
-              <p className="text-sm text-gray-600">{post.description}</p>
-              <div className="flex w-full items-center justify-end">
-                <NextLink
-                  className="text-xs text-gray-600 underline"
-                  href={`/blog/${post.slug}`}
-                >
-                  Read Article
-                </NextLink>
+      {blogPosts
+        .slice(0, 4)
+        .sort(
+          (a, b) =>
+            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+        )
+        .map((post) => {
+          const formattedDate = formatDate(post.date);
+          const formattedUpdatedAt = formatDate(post.updatedAt);
+          return (
+            <div
+              key={post.id}
+              className="flex items-start gap-4 max-[700px]:flex-col"
+            >
+              {post.image && (
+                <Image
+                  src={post.image.src}
+                  alt={post.image.alt}
+                  width={150}
+                  height={150}
+                  className="shrink-0 rounded"
+                  loading="eager"
+                />
+              )}
+              <div className="flex flex-col gap-2">
+                <h3 className="font-semibold">{post.title}</h3>
+                <div className="flex items-center gap-1 max-[700px]:flex-col max-[700px]:items-start">
+                  <span className="text-xs text-gray-500 dark:text-gray-400">
+                    Published on {formattedDate}
+                  </span>
+                  <span className="max-[700px]:hidden">|</span>
+                  <span className="text-xs text-gray-500 dark:text-gray-400">
+                    Updated on {formattedUpdatedAt}
+                  </span>
+                </div>
+                <p className="text-sm text-gray-600">{post.description}</p>
+                <div className="flex w-full items-center justify-end">
+                  <NextLink
+                    className="text-xs text-gray-600 underline"
+                    href={`/blog/${post.slug}`}
+                  >
+                    Read Article
+                  </NextLink>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
     </div>
   );
 }
