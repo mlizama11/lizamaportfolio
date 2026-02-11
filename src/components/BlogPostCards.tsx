7a2f748e9c0f1f04867fae74028bdea4d0dffae4
Mauricio Lizama <mlizamaoliger@gmail.com>
2026-02-11 08:50:42 +0100

    update sort in all blog cards

--- src/components/BlogPostCards.tsx
+++ src/components/BlogPostCards.tsx
@@ -6,17 +6,21 @@ export function BlogPostCards({ blogPosts }: BlogPostsListProps) {
   return (
     <div className="flex flex-col gap-3">
       <div className="grid gap-8 md:grid-cols-2">
-        {blogPosts.map((singlePost) => {
-          if (!singlePost.image) return null;
-          const { src, alt, width, height } = singlePost.image;
-          return (
-            <BlogPostCard
-              key={singlePost.id}
-              singlePost={singlePost}
-              image={{ src, alt, width, height }}
-            />
-          );
-        })}
+        {blogPosts
+          .sort(
+            (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+          )
+          .map((singlePost) => {
+            if (!singlePost.image) return null;
+            const { src, alt, width, height } = singlePost.image;
+            return (
+              <BlogPostCard
+                key={singlePost.id}
+                singlePost={singlePost}
+                image={{ src, alt, width, height }}
+              />
+            );
+          })}
       </div>
     </div>
   );
