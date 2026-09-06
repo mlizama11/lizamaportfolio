182c18fc45adda686493223153ef34c58505332d
Mauricio Lizama <mlizamaoliger@gmail.com>
2026-09-06 18:19:14 +0200

    fix: enhance About section with additional personal and professional details

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,6 +11,19 @@ export default function About() {
         communications. I’m passionate about sustainable development, nature
         conservation, politics, and ecotourism.
       </p>
+      <p>
+        I was born in Costa Rica (🇨🇷), and I also have family roots in Chile
+        (🇨🇱) and France (🇫🇷). In the past few years, from late 2021 to mid-2025,
+        I worked at I Planted GmbH in Cologne. From late 2020 to 2021, I studied
+        and graduated as a full-stack developer (MERN stack) at the Digital
+        Career Institute (DCI) in Germany (🇩🇪).
+      </p>
+      <p>
+        Currently, I am learning German with the goal of reaching the B2 level
+        and taking courses on master.dev to improve my frontend and backend
+        skills in topics such as Python, DevOps, PHP, AI, Angular, Vue, among
+        others.
+      </p>
     </Section>
   );
 }
