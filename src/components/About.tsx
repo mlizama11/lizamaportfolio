5101855177e50b99583608d0cc0a006d5343496a
Mauricio Lizama <mlizamaoliger@gmail.com>
2026-09-06 10:01:12 +0200

    fix: update About section to reflect current experience and skills

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,13 +6,10 @@ export default function About() {
       <SectionTitle className="text-3xl font-bold">About Me</SectionTitle>
       <p>
         I am a Full Stack Developer skilled in <b>TypeScript</b>, <b>React</b>{' '}
-        and <b>NestJS</b>. With four years of hands-on experience in software
-        development, I understand that effective teamwork and a sense of
-        ownership are crucial for creating scalable, high-performance products
-        that are user-friendly and responsive. Furthermore, I am a professional
-        journalist driven by a keen interest in new technologies. My passions
-        include sustainable development, nature conservation, politics, and
-        ecotourism.
+        and <b>NestJS</b>, with professional experience in software development
+        since 2021. Also, as a professional journalist, I have strong skills in
+        communications. I’m passionate about sustainable development, nature
+        conservation, politics, and ecotourism.
       </p>
     </Section>
   );
